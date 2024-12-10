b293091cbded6973751ebc281f02380762a8c682
Shaked Sapir <shakedsapir@Shakeds-MBP.lan>
2024-12-10 22:19:30 +0200

    parsing image predicates and GT predicates to pddl-plus-parser is working. now needs a lot of code refactoring

--- src/domains/blocks/blocks_images/algorithm.py
+++ src/domains/blocks/blocks_images/algorithm.py
@@ -1,5 +1,7 @@
 import json
 import os
+from pathlib import Path
+from typing import Tuple, Dict, List
 
 import cv2
 from PIL import Image
@@ -8,8 +10,60 @@ import pddlgym
 from pddlgym.core import _select_operator
 from pddlgym.rendering.blocks import _block_name_to_color
 
+from src.action_model.GYM2SAM_parser import create_observation_from_trajectory
+from src.fluent_classification.colors import NormalizedRGB
+from src.fluent_classification.contours import get_image_predicates
+from pddl_plus_parser.lisp_parsers import DomainParser, ProblemParser
+from pddl_plus_parser.models import (
+    State,
+    GroundedPredicate,
+    Observation,
+    ActionCall,
+    Domain, Problem, PDDLObject, SignatureType
+)
 
-def alg(problem, num_steps=1000):
+def parse_image_predicate_to_gym(predicate_str: str, is_holding_in_image: bool) -> str:
+    """
+    Parse a predicate extracted from an image to pddlgym format.
+    :param predicate_str: the string representing the predicate, e.g. `holding(e:block)`
+    :param is_holding_in_image: whether the predicate holds in the image it was extracted from
+    :return: updated string representing the predicate
+    """
+    return predicate_str if is_holding_in_image else f"Not{predicate_str}"
+
+
+def is_positive_gym_predicate(predicate_str: str) -> bool:
+    return 'Not' not in predicate_str
+
+
+def create_imaged_trajectory_info(ground_actions: List[str], object_name_to_color: Dict[str, str]):
+    imaged_trajectory_info = []
+    for i, action in enumerate(ground_actions):
+
+        current_state_image = cv2.imread(f"blocks_images/state_{i:04d}.png")  # in BGR format
+        current_state_image_predicates = get_image_predicates(current_state_image, object_name_to_color)
+        current_state_image_pddl_predicates: List[str] = [parse_image_predicate_to_gym(pred, holds_in_image) for pred, holds_in_image in
+                                      current_state_image_predicates.items()]
+
+        next_state_image = cv2.imread(f"blocks_images/state_{i+1:04d}.png")  # in BGR format
+        next_state_image_predicates = get_image_predicates(next_state_image, object_name_to_color)
+        next_state_image_pddl_predicates: List[str] = [parse_image_predicate_to_gym(pred, holds_in_image) for
+                                                          pred, holds_in_image in
+                                                          next_state_image_predicates.items()]
+        imaged_trajectory_info.append({
+            "step": i+1,
+            "current_state": {
+                "literals": [pred for pred in current_state_image_pddl_predicates if is_positive_gym_predicate(pred)]
+            },
+            "ground_action": action,
+            "next_state": {
+                "literals": [pred for pred in next_state_image_pddl_predicates if is_positive_gym_predicate(pred)]
+            },
+        })
+    return imaged_trajectory_info
+
+
+def alg(num_steps=1000):
     """
     This the main workflow of predicates classification within an image in the blocks world.
     :param problem: the problem instance we desire to make trajectory from
@@ -17,7 +71,8 @@ def alg(problem, num_steps=1000):
     :return:
     """
 
-    #TODO: check how to apply for a specific problem file
+    print("alg started")
+    #TODO: check how to apply for a specific problem file so we can use it later as a param to the Observation creation
     env = pddlgym.make("PDDLEnvBlocks-v0")
 
     obs, info = env.reset()
@@ -31,41 +86,38 @@ def alg(problem, num_steps=1000):
     log_file_path = os.path.join(output_dir, "states_and_actions.json")
     states_and_actions = []
 
-    # Save the initial state
-    # Save the initial state
-    initial_entry = {
-        "step": 0,
-        "action": "initial_state",
-        "state": str(obs)
-    }
-    states_and_actions.append(initial_entry)
     # Render the environment and save the image
     img = env.render(mode='rgb_array')
     img_pil = Image.fromarray(img)
     img_pil.save(os.path.join(output_dir, f"state_{0:04d}.png"))
 
-    # extract colors:  the mapping is generated at problem initialization
-    block_name_to_color = {str(obj): color for obj, color in _block_name_to_color.items()}
-
     # Run 1000 random moves and save the images
-    for i in range(1, num_steps):
+    for i in range(1, num_steps+1):
         obs = new_obs
         # Sample a random valid action from the set of valid actions
 
         while new_obs == obs:
             action = env.action_space.sample(obs)
-            new_obs = env.step(action)[0]  # new_obs holds the "next_state", i.e. the state resulting in executing the action
+            new_obs, _, done, _, _ = env.step(action)
 
         # Record the state and action
         state_action_entry = {
             "step": i,
-            "current_state": str(obs),
+            "current_state": {
+                "literals": [str(literal) for literal in obs.literals],
+                "objects": [str(obj) for obj in obs.objects],
+                "goal": [str(literal) for literal in obs.goal.literals]
+            },
             "ground_action": str(action),
 
             #TODO later: the _select_operator seems to make it a "safe" action, but the blocksworld is not a domain prone to unsafety - discuss with Roni
             "operator_object_assignment": _select_operator(obs, action, env.domain)[1],
             "lifted_preconds": str(env.domain.operators['pick-up'].preconds.literals),
-            "ground_resulted_state": str(new_obs)
+            "next_state": {
+                "literals": [str(literal) for literal in new_obs.literals],
+                "objects": [str(obj) for obj in new_obs.objects],
+                "goal": [str(literal) for literal in new_obs.goal.literals]
+            }
         }
 
         states_and_actions.append(state_action_entry)
@@ -75,9 +127,8 @@ def alg(problem, num_steps=1000):
         img_pil = Image.fromarray(img)
         img_pil.save(os.path.join(output_dir, f"state_{i:04d}.png"))
 
-        # TODO: Reset the environment if a terminal state is reached
-        # if done:
-        #     env.reset()
+        if done:
+            break
 
     # Save the states and actions to the log file
     with open(log_file_path, 'w') as log_file:
@@ -87,9 +138,41 @@ def alg(problem, num_steps=1000):
     print(f"Images saved to the directory '{output_dir}'")
     print(f"States and actions log saved to '{log_file_path}'")
 
+    """
+    extracting colors of objects from trajectory so we can detect the objects in the image
+    """
+    # extract colors:  the mapping is generated at problem initialization
+    object_name_to_color: Dict[str, NormalizedRGB] = {
+        **{str(obj): color for obj, color in _block_name_to_color.items()},
+        "robot:robot": (0.4, 0.4, 0.4),
+        "table:table": (0.5, 0.2, 0.0)
+    }
 
-"""
-extract predicates for each picture in trajectory, automatically managing the coloring stuff.
-notice that only blocks get colors in the mapping, the colors of table and robot are unique and are the same
-in all problems, so we can map them ahead.
-"""
+    print(f"Object name to color map: {object_name_to_color}")
+
+    pddl_plus_blocks_domain: Domain = DomainParser(Path("../blocks.pddl")).parse_domain()
+    pddl_plus_blocks_problem: Problem = ProblemParser(Path("../problem9.pddl"),
+                                               pddl_plus_blocks_domain).parse_problem()
+    GT_observation: Observation = create_observation_from_trajectory(states_and_actions, pddl_plus_blocks_domain,
+                                                                  pddl_plus_blocks_problem)
+
+    # Output the resulting Observation object
+    print("printing GT observation:")
+    for component in GT_observation.components:
+        print(str(component))
+
+    print("*****************************")
+
+    grounded_actions = [step["ground_action"] for step in states_and_actions]
+    imaged_trajectory_info = create_imaged_trajectory_info(grounded_actions, object_name_to_color)
+    imaged_observation: Observation = create_observation_from_trajectory(imaged_trajectory_info, pddl_plus_blocks_domain, pddl_plus_blocks_problem)
+
+    # Output the resulting Observation object
+    print("printing imaged observation:")
+    for component in imaged_observation.components:
+        print(str(component))
+
+    print("*****************************")
+
+if __name__ == "__main__":
+    alg(num_steps=15)
--- src/fluent_classification/colors.py
+++ src/fluent_classification/colors.py
@@ -1,17 +1,20 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import cv2
+import numpy as np
+
+NormalizedRGB = Union[Tuple[float, float, float], np.array]
+IntRGB = Union[Tuple[int, int, int], np.array]
+
+def to_int_rgb(normalized_rgb_triplet: NormalizedRGB) -> IntRGB:
+    return tuple(round(v*255) for v in normalized_rgb_triplet)
 
 
-def to_int_rgb(normalized_rgb_tuple: Tuple[float, float, float]) -> Tuple[int, ...]:
-    return tuple(round(v*255) for v in normalized_rgb_tuple)
-
-
-def find_exact_rgb_color_mask_with_compare(image, rgb_tuple):
+def find_exact_rgb_color_mask(image, int_rgb_triplet: IntRGB):
     # Create masks for each channel comparison
-    mask_r = cv2.compare(image[:, :, 2], rgb_tuple[0], cv2.CMP_EQ)  # Red channel
-    mask_g = cv2.compare(image[:, :, 1], rgb_tuple[1], cv2.CMP_EQ)  # Green channel
-    mask_b = cv2.compare(image[:, :, 0], rgb_tuple[2], cv2.CMP_EQ)  # Blue channel
+    mask_r = cv2.compare(image[:, :, 2], int_rgb_triplet[0], cv2.CMP_EQ)  # Red channel
+    mask_g = cv2.compare(image[:, :, 1], int_rgb_triplet[1], cv2.CMP_EQ)  # Green channel
+    mask_b = cv2.compare(image[:, :, 0], int_rgb_triplet[2], cv2.CMP_EQ)  # Blue channel
 
     # Combine masks to ensure all channels match
     mask = cv2.bitwise_and(mask_r, cv2.bitwise_and(mask_g, mask_b))
